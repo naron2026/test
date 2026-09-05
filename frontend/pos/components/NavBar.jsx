27b48e0ab4fdad9cfaaee58fc726edb9e46bbd37
ai video <aiv60400@gmail.com>
2026-09-05 21:21:10 +0700

    update navbar.

--- frontend/pos/components/NavBar.jsx
+++ frontend/pos/components/NavBar.jsx
@@ -2,25 +2,15 @@ function NavBar() {
   return (
     <div className="navbar bg-green-600 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl text-white">daisyUI</a>
+        <a className="btn btn-ghost text-xl text-white">MASTER POS</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Link</a>
+            <a>POS</a>
           </li>
           <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="bg-base-100 rounded-t-none p-2">
-                <li>
-                  <a>Link 1</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-              </ul>
-            </details>
+            <a>ADMIN</a>
           </li>
         </ul>
       </div>
